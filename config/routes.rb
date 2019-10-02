9ae2fe3f015869e94bfe862eeed3dc971457c554
koh-suzuki <k.s_coward_0629@icloud.com>
2019-10-02 06:30:11 +0000

    signup-user Finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'static_page#top'
   get '/signup', to: 'users#new'
+  
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
